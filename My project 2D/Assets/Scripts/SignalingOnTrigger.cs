6c4080dc10044591a276f3d4ecfc103f62abe5e6
Draft <140250607+Sergey-exe@users.noreply.github.com>
2024-04-20 12:15:11 +0300

    Alarm against scammer personal adjustment

--- My project 2D/Assets/Scripts/SignalingOnTrigger.cs
+++ My project 2D/Assets/Scripts/SignalingOnTrigger.cs
@@ -2,19 +2,44 @@ using UnityEngine;
 
 public class SignalingOnTrigger : MonoBehaviour
 {
-    [SerializeField] private Signaling _signaling;
+    [SerializeField] private AudioSource _audioSource;
+    [SerializeField] private float _speed;
+
+    private bool _isPlaying;
+
+    private void Update()
+    {
+        float maxSound = 1.0f;
+
+        if (_isPlaying)
+        {
+            _audioSource.volume = Mathf.MoveTowards(_audioSource.volume, maxSound, _speed * Time.deltaTime);
+        }
+        else
+        {
+            _audioSource.volume = Mathf.MoveTowards(_audioSource.volume, maxSound, -_speed * Time.deltaTime);
+        }
+
+        if(_audioSource.volume == 0)
+        {
+            _audioSource.Stop();
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.GetComponent<HeroKnight>())
-            _signaling.StartSignaling();
+        {
+            _audioSource.Play();
+            _isPlaying = true;
+        }
+            
+        
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        int reverseDirection = -1;
-
         if (collision.GetComponent<HeroKnight>())
-            _signaling.ChangeVolume(reverseDirection);
+            _isPlaying = false;
     }
 }
