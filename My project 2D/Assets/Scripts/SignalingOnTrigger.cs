2dc1f3e52b36f31b7dcb4683495f8343237a019e
Draft <140250607+Sergey-exe@users.noreply.github.com>
2024-04-22 15:41:29 +0300

    Anti-fraud alarm

--- My project 2D/Assets/Scripts/SignalingOnTrigger.cs
+++ My project 2D/Assets/Scripts/SignalingOnTrigger.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class SignalingOnTrigger : MonoBehaviour
@@ -5,39 +7,59 @@ public class SignalingOnTrigger : MonoBehaviour
     [SerializeField] private AudioSource _audioSource;
     [SerializeField] private float _speed;
 
-    private bool _isSoundPlaying;
-
-    private void Update()
-    {
-        float maxSound = 1.0f;
-
-        if (_isSoundPlaying)
-        {
-            _audioSource.volume = Mathf.MoveTowards(_audioSource.volume, maxSound, _speed * Time.deltaTime);
-        }
-        else
-        {
-            _audioSource.volume = Mathf.MoveTowards(_audioSource.volume, maxSound, -_speed * Time.deltaTime);
-        }
-
-        if(_audioSource.volume == 0)
-        {
-            _audioSource.Stop();
-        }
-    }
+    private Coroutine _coroutine = null;
+    private bool _hasRogue;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.GetComponent<HeroKnight>())
         {
             _audioSource.Play();
-            _isSoundPlaying = true;
+
+            _hasRogue = true;
+
+            if (_coroutine != null)
+                StopCoroutine(_coroutine);
+
+            _coroutine = StartCoroutine(CounterCountValue());
         }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.GetComponent<HeroKnight>())
-            _isSoundPlaying = false;
+        {
+            if (_coroutine != null)
+                StopCoroutine(_coroutine);
+
+            _coroutine = StartCoroutine(CounterCountValue(-1));
+        }    
+    }
+
+    private IEnumerator CounterCountValue(int direction = 1, float delay = 0.1f)
+    {
+        var wait = new WaitForSeconds(delay);
+
+        int minValue = 0;
+        int maxValue = 1;
+
+        while (_hasRogue)
+        {
+            _audioSource.volume = Mathf.MoveTowards(_audioSource.volume, maxValue, direction * _speed * Time.deltaTime);
+
+            if (_audioSource.volume == minValue)
+            {
+                _audioSource.Stop();
+                _hasRogue = false;
+
+                StopCoroutine(_coroutine);
+            }
+            else if (_audioSource.volume == maxValue)
+            {
+                StopCoroutine(_coroutine);
+            }
+
+            yield return wait;
+        }
     }
 }
